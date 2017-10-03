73c978d009c44a2327a5ec246926a3d7469d6893
paul manot <paulmanot@gmail.com>
2017-10-03 22:03:51 +1100

    Add static pages with test suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about' # creates a route to the about page via the StaticPages controller
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
